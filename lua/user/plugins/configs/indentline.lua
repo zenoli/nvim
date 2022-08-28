17cb01f28d16050cafa35b0188b83211fc010733
olivier <olibitter@gmail.com>
2022-08-28 14:09:29 +0200

    Don't show indenteline on vimwiki ft

--- lua/user/plugins/configs/indentline.lua
+++ lua/user/plugins/configs/indentline.lua
@@ -4,6 +4,7 @@ return {
         require("indent_blankline").setup {
             show_current_context = true,
             show_current_context_start = false,
+            filetype_exclude = { "vimwiki" }
         }
 
         -- Mappings
