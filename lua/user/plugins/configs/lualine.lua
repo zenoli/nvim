58c08961623bd421d56e838765e0084cef878653
olivier <olibitter@gmail.com>
2022-08-29 23:51:59 +0200

    remove statusline for neo-tree

--- lua/user/plugins/configs/lualine.lua
+++ lua/user/plugins/configs/lualine.lua
@@ -1,6 +1,14 @@
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
-        require("lualine").setup()
+        require("lualine").setup {
+            options = {
+                disabled_filetypes = {
+                    statusline = { "neo-tree" },
+                    winbar = {},
+                },
+                ignore_focus = { "neo-tree" }
+            }
+        }
     end
 }
