52d53619c9582986ece125ea2c2b69ef75abfbf3
olivier <olibitter@gmail.com>
2022-08-29 12:14:22 +0200

    Add fd requirement note

--- lua/user/plugins/configs/neo-tree/init.lua
+++ lua/user/plugins/configs/neo-tree/init.lua
@@ -3,8 +3,9 @@ return {
     branch = "v2.x",
     requires = {
         "nvim-lua/plenary.nvim",
-        "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+        "kyazdani42/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
+        -- NOTE: The fuzzy search only works reliably when 'fd' is installed.
     },
     after = "nvim-window-picker",
     config = function ()
