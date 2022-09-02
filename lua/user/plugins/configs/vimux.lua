590241059791030827a5b8fc7cef9a0e4dbae6b1
olivier <olibitter@gmail.com>
2022-09-02 10:15:51 +0200

    Rewrite vimux config in lua

--- lua/user/plugins/configs/vimux.lua
+++ lua/user/plugins/configs/vimux.lua
@@ -1,6 +1,12 @@
 return {
     "benmills/vimux",
     config = function()
-        vim.cmd('source ~/.config/nvim/plug-configs/vimux.vim')
+        local map = require "user.utils".map
+
+        map("n", "<leader>vp", ":VimuxPromptCommand<cr>")
+        map("n", "<leader>vl", ":VimuxRunLastCommand<cr>")
+        map("n", "<leader>vi", ":VimuxInspectRunner<cr>")
+        map("n", "<leader>vq", ":VimuxCloseRunner<cr>")
+        map("n", "<leader>vz", ":VimuxZoomRunner<cr>")
     end
 }
