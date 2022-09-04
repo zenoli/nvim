6e9c434c8fefda08bd569abecdb721757c490956
olivier <olibitter@gmail.com>
2022-09-04 18:26:50 +0200

    Make neo-tree unstage icon non-italic

--- lua/user/plugins/configs/onedark.lua
+++ lua/user/plugins/configs/onedark.lua
@@ -8,11 +8,22 @@ return {
         }
         -- onedark.load()
         vim.cmd [[colorscheme onedark]]
+
+        -- Less obtrusive highlight color
         vim.api.nvim_set_hl(0, "Search", { link = "Visual" })
         vim.api.nvim_set_hl(0, "IncSearch", { link = "Visual" })
+
+        -- More red in onedark
         vim.api.nvim_set_hl(0, "TSVariable", { link = "TSParameter" })
+
+        -- More subtle indent lines
         vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { link = "LineNr" })
         vim.api.nvim_set_hl(0, "IndentBlanklineChar", { link = "VertSplit" })
+
+        -- Clean neo-tree separation border
         vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { link = "NeoTreeEndOfBuffer" })
+
+        -- Make unstage icon non-italic
+        vim.api.nvim_set_hl(0, "NeoTreeGitUnstaged", { link = "NeoTreeGitDeleted" })
     end
 }
