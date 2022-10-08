521d0dbd8a55273bf8baaf2da59067a3e87a5e93
olivier <olibitter@gmail.com>
2022-10-08 19:44:07 +0200

    Add bash formatter and codeactions

--- lua/user/plugins/configs/null-ls.lua
+++ lua/user/plugins/configs/null-ls.lua
@@ -1,12 +1,19 @@
 return {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
+        local null_ls = require "null-ls"
         require("null-ls").setup {
             sources = {
-                -- require("null-ls").builtins.diagnostics.luacheck,
-                require("null-ls").builtins.formatting.stylua,
-                require("null-ls").builtins.diagnostics.eslint,
-                -- require("null-ls").builtins.completion.spell,
+                -- Lua
+                null_ls.builtins.formatting.stylua,
+
+                -- Javascript
+                null_ls.builtins.diagnostics.eslint,
+
+                -- Shell/Bash
+                null_ls.builtins.code_actions.shellcheck,
+                null_ls.builtins.formatting.shfmt,
+                null_ls.builtins.hover.printenv,
             },
         }
     end,
