6d2b5de0f5fd8db506538cc6078812aadf3fcafc
olivier <olibitter@gmail.com>
2022-10-14 14:08:43 +0200

    Lazy load vimux

--- lua/user/plugins/configs/vimux.lua
+++ lua/user/plugins/configs/vimux.lua
@@ -1,12 +1,20 @@
 return {
     "benmills/vimux",
-    config = function()
-        local map = require "user.utils".map
+    key = {
+        "<leader>vp",
+        "<leader>vl",
+        "<leader>vi",
+        "<leader>vq",
+        "<leader>vz",
+    },
+    cmd = "Vimux*",
+    setup = function()
+        local map = require("user.utils").map
 
         map("n", "<leader>vp", ":VimuxPromptCommand<cr>")
         map("n", "<leader>vl", ":VimuxRunLastCommand<cr>")
         map("n", "<leader>vi", ":VimuxInspectRunner<cr>")
         map("n", "<leader>vq", ":VimuxCloseRunner<cr>")
         map("n", "<leader>vz", ":VimuxZoomRunner<cr>")
-    end
+    end,
 }
