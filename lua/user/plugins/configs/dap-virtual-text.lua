c1e74d97379716dcb8ec2678f4b298c34ceeee21
olivier <olibitter@gmail.com>
2022-12-14 13:04:18 +0100

    Disable all frames dap-virtual-text

--- lua/user/plugins/configs/dap-virtual-text.lua
+++ lua/user/plugins/configs/dap-virtual-text.lua
@@ -2,6 +2,8 @@ return {
     "theHamsta/nvim-dap-virtual-text",
     after = "nvim-dap",
     config = function()
-        require("nvim-dap-virtual-text").setup()
+        require("nvim-dap-virtual-text").setup {
+            all_frames = false
+        }
     end
 }
